ae7943f4647eef9c882fdfbb415b6802b6bdd922
Ryan Sheehy <ryansheehy0@gmail.com>
2023-07-04 14:33:49 -0700

    Adds all functionality to script.js, removes past, present, and future examples in index.html, and adds .center and other settings in style.css.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+## Description
+A web app that allows you to keep track of your past, present, and future events throughout the day.
+
+## Usage
+Type in your event for the hour. Click the save button to the right and it will save it to local storage.
+
+![screenshot1](./Assets/screenshots/screenshot1.png)
+
+You can check out the website through this [link](https://ryansheehy0.github.io/Module_5_Challenge/).
+
+## Source code
+
+The source code can be found within the folder docs.
--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,85 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var saveMessageTimer
+function showThenHideSaveMessage(){
+	// show
+	$("#saveMessage").css("visibility", "visible")
+	// clear previous timer
+	clearTimeout( saveMessageTimer )
+	// new timer
+	saveMessageTimer = setTimeout(function() {
+		$("#saveMessage").css("visibility", "hidden")
+	}, 1.5 * 1000)
+}
+
+function saveButtonClickedEvent(hour){
+	let saveButton = $(`#hour-${hour} > button`)
+	let textArea = $(`#hour-${hour} > textarea`)
+	saveButton.click(function () {
+		localStorage.setItem(`${hour}`, textArea.val())
+		showThenHideSaveMessage()
+	})
+}
+
+function loadLocalStorage(hour) {
+	let textArea = $(`#hour-${hour} > textarea`)
+	let localStorageValue = localStorage.getItem(`${hour}`)
+	if(localStorageValue !== null){
+		textArea.val(localStorageValue)
+	}
+}
+
+function forEachTimeSlot(func){ // function takes in the hour
+	for(let i = 9; i !== 6; i++){
+		if( i % 13 === 0){
+			i = 1
+		}
+		func(i)
+	}
+}
+
+function appendTimeSlot(hour){
+	let slotContainer = $(".container-fluid")
+	let militaryHour = hour < 6 ? hour + 12 : hour
+	let currentMilitaryHour = parseInt(dayjs().format("HH"))
+	let pastPresentFuture
+	if(militaryHour < currentMilitaryHour) {
+		pastPresentFuture = "past"
+	}else if( militaryHour === currentMilitaryHour){
+		pastPresentFuture = "present"
+	}else{
+		pastPresentFuture = "future"
+	}
+	let newTimeSlot = $(`
+		<div id="hour-${hour}" class="row time-block ${pastPresentFuture}">
+			<div class="col-2 col-md-1 hour text-center py-3">${(hour > 6 && hour !== 12) ? hour+"AM" : hour+"PM"}</div>
+			<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+			<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+				<i class="fas fa-save" aria-hidden="true"></i>
+			</button>
+		</div>
+	`)
+	slotContainer.append(newTimeSlot)
+}
+
+$(function () { // Makes sure html elements are loaded before referencing them
+	// Add all the time slots
+	forEachTimeSlot( appendTimeSlot )
+	// Load the local storage for all time slots
+	forEachTimeSlot( loadLocalStorage )
+	// Add save button click event listener to all the time slots
+	forEachTimeSlot( saveButtonClickedEvent )
+
+	// Add day of the week
+	let weekday = dayjs().format('dddd');
+	let month = dayjs().format('MMMM');
+	let dayOfMonth = dayjs().format('D');
+	if(dayOfMonth == 1 || dayOfMonth == 21 || dayOfMonth == 31){
+		dayOfMonth += "st"
+	}else if(dayOfMonth == 2 || dayOfMonth == 22){
+		dayOfMonth += "nd"
+	}else if(dayOfMonth == 3 || dayOfMonth == 23){
+		dayOfMonth += "rd"
+	}else{
+		dayOfMonth += "th"
+	}
+	$("#currentDay").text(`${weekday}, ${month} ${dayOfMonth}`)
 });
